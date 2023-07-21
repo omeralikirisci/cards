d71856bef2d1f2eb774a8fc24aa3dc8d5c391582
omerallu <104915515+omerallu@users.noreply.github.com>
2023-07-21 03:51:13 +0200

    Add files via upload

---
+++ cards/deck.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+// create a new type of 'deck'
+//which is a slice of strings
+type deck []string
+
+func newDeck() deck {
+	cards := deck{}
+
+	cardSuits := []string{"Spades", "Diamonds", "Hearts", "Clubs"}
+	cardValues := []string{"Ace", "Two", "Three", "Four"}
+
+	for _, suit := range cardSuits {
+		for _, value := range cardValues {
+			cards = append(cards, value+" of "+suit)
+		}
+	}
+
+	return cards
+}
+
+func (d deck) print() {
+	for i, card := range d {
+		fmt.Println(i, card)
+	}
+}
---
+++ cards/main.go
@@ -0,0 +1,9 @@
+package main
+
+func main() {
+	//var card string = "Ace of Spades"
+	cards := newDeck()
+
+	cards.print()
+
+}
